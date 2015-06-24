079d02e6a50dd1198fdbd1f4b0d54b0be8c3c2f9
awegrzyn <adam.wegrzynek@cern.ch>
2015-06-24 14:21:55 +0200

    init of GUI directory with PHP select_stream test

---
+++ GUI/client.php
@@ -0,0 +1,9 @@
+<?php
+	$fp = stream_socket_client("tcp://127.0.0.1:4444", $errno, $errstr, 30);
+    while(true) {
+    	fwrite($fp, "gowno gowno ".rand(1, 100));
+    	sleep(10);
+    	var_dump(fread($fp, 1024));
+    }
+    fclose($fp);
+    ?>
