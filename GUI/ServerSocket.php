f717263e08ad92617828cc78fe3c882a69acf488
awegrzyn <adam.wegrzynek@cern.ch>
2015-06-24 17:53:16 +0200

    Handshake class

--- GUI/ServerSocket.php
+++ GUI/ServerSocket.php
@@ -25,7 +25,7 @@ class ServerSocket {
 		for (;;) {
 			$this->checkNewClients();
 			$this->readClientSockets();
-			$this->zmqh->sendMessage();
+			//$this->zmqh->sendMessage();
 			//$this->zmqh->getMessage();
 			usleep(1000000);
 		}
@@ -40,27 +40,51 @@ class ServerSocket {
     	} elseif ($no_changed > 0) {
     		$socket = stream_socket_accept($this->serverSocket);
     		if ($socket) {
+    			//$this->handshake($socket);
     			array_push($this->clients, $socket);
     		} else {
     			echo "ERROR: niepoprawy socket_accept (nie powinno sie do zdazyc)";
     		}
     	}
 	}
+	private function handshake(&$socket) {
+		try {
+			if (($read = $this->read($socket)) == false) {
+				echo("Cannot read handshake");
+			}
+			print_r($read);
+			$h = new Handshake($read);
+			$response = $h->parseHandshake();
+			
+
+		} catch (\Exception $e) {
+			return false;
+		}
+		return true;
+	}
+	protected function read(&$socket, $toRead = 1500) {
+		$return = stream_socket_recvfrom($socket, $toRead);
+        return $return;
+    }
 	private function readClientSockets() {
-		if (count($this->clients) <= 0) {
+		echo count($this->clients).PHP_EOL;
+		if (empty($this->clients)) {
 			return;
 		}
 		$write  = NULL;
 		$except = NULL;
-		$clientsArray = $this->clients;
+		$clientsArray = array_filter($this->clients);
+		//print_r($this->client);
 		if (($no_changed = stream_select($clientsArray, $write, $except, 0)) === false) {
         	//no awaiting connection
     	} elseif ($no_changed > 0) {
     		foreach($clientsArray as $socket) {
-		        $data = fread($socket, 128);
+		        if (($data = $this->read($socket)) === false) {
+		        	echo "failed to read socket".PHP_EOL;
+		        }
 				echo "Reading out data from id ".intval($socket).": '".$data."'".PHP_EOL;
 		        if(!$data) {
-		            unset($client_socks[array_search($sock, $this->clients)]);
+		            unset($this->clients[array_search($socket, $this->clients)]);
 		            @fclose($socket);
 		            echo "A client id ".intval($socket)." disconnected. Now there are total ". count($this->clients) . " clients".PHP_EOL;
 		            continue;
