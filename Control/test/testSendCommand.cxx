b058b28819361699f99ed7ea0cf7ab3cb2f58ee0
sy-c <o2.syc@cern.ch>
2015-08-07 17:35:12 +0200

    state machine added

---
+++ Control/test/testSendCommand.cxx
@@ -0,0 +1,59 @@
+#include <StateMachine.h>
+#include <unistd.h>
+
+//using namespace StateMachine;
+
+
+
+int main(int argc, char *argv[]) {
+
+  const char *objName="test";
+  const char *cmd="MOVE";
+  int nIter=10;
+  
+  if (argc>1) {
+    objName=argv[1];
+  }
+  if (argc>2) {
+    cmd=argv[2];
+  }
+  if (argc>3) {
+    nIter=atoi(argv[3]);
+  }
+
+  try {
+    DirectoryConfig dCfg("localhost:2181");
+    Directory d(dCfg);
+
+/*  ControlClient *obj[100];
+    for (int i=0;i<100;i++) {
+      char a[100];
+      snprintf(a,100,"test%03d",i);
+      obj[i]=new ControlClient(a,&d);
+    }
+    for (int i=0;i<1;i++) {
+      char cmd[100];
+      snprintf(cmd,100,"MOVE%0d",i);
+      obj[i%100]->sendCommand(cmd);
+    }
+*/
+
+    ControlClient obj(objName,&d);
+    obj.sendCommand(cmd);
+    for (int i=0;i<nIter;i++) {
+      obj.sendCommand(cmd);
+      /*
+      char cmdl[100];
+      snprintf(cmdl,100,"%s%0d",cmd,i);
+      obj.sendCommand(cmdl);
+      */
+    }
+    sleep(10);
+    printf("done\n");
+
+  }
+  catch (const char *e) {
+    printf("error : %s\n",e);
+  }
+  return 0;
+}
--- Control/test/testStateMachine.cxx
+++ Control/test/testStateMachine.cxx
@@ -3,19 +3,64 @@
 
 //using namespace StateMachine;
 
-int main() {
+
+class myStateMachine:public ControlObject {
+  public:
+  int i=0;
+  myStateMachine(const std::string objectName, Directory * dir):ControlObject(objectName,dir) {
+    setState("READY");
+  };
+  ~myStateMachine() {
+  };
+  void executeCommand(const std::string command){
+    i++;
+    char buf[20];
+    snprintf(buf,20,"STATE-%03d",i);
+    setState(buf);
+  };
+};
+
+
+
+int main(int argc, const char *argv[]) {
+  
+  const char *objName="test";
+  if (argc>1) {
+    objName=argv[1];
+  }
+    
+    
   try {
     DirectoryConfig dCfg("localhost:2181");
     Directory d(dCfg);
     
-    ControlObject obj("test",&d);
+    /*d.CreateNode("/oo"); 
+    
+    for (int i=0;i<100;i++) {
+      char a[100];
+      snprintf(a,100,"test%03d",i);
+      ControlObject *obj;
+      obj=new ControlObject(a,&d);
+      obj->setState("READY");
+    }
+    */
+    
+    /*ControlObject obj(objName,&d);
     obj.setState("READY");
-    sleep(10);
+    */
+    
+    myStateMachine obj(objName,&d);
+    
+    sleep(100);
     printf("done\n");
 
   }
   catch (const char *e) {
     printf("error : %s\n",e);
   }
+  catch (int e) {
+    printf("error : %d\n",e);
+  }
+  
   return 0;
 }
