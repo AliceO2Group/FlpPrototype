dc691f40db3390004a4d7a3dbdf67b654e9a184c
sy-c <o2.syc@cern.ch>
2015-08-20 17:15:43 +0200

    added interface for state callbacks

--- Control/include/StateMachine.h
+++ Control/include/StateMachine.h
@@ -34,13 +34,14 @@ class DirectoryConfig {
 };
 
 
-// data structure to hold information to setup callbacks. It consists of:
+// data structure to hold information to setup node callbacks. It consists of:
 // - a function to be called
 // - a user-configurable context parameter which will be provided to the function
 class NodeCallback {
 public:
-  void (*f_callback)(const std::string value, void *context);  // function prototype for node update callback. function will be called with new node value, and 'context' pointer provided at subscribe time.
-  void *context;                                               // parameter to be given to the function
+  enum NodeEventType {NodeCreated,NodeDestroyed,NodeUpdated};                       // the event type creating the callback
+  void (*f_callback)(const std::string value, void *context, NodeCallback::NodeEventType event);  // function prototype for node update callback. function will be called with new node value, and 'context' pointer provided at subscribe time.
+  void *context;                                                                    // parameter to be given to the function
 };
 
 /// creates an access to the central directory
@@ -57,9 +58,11 @@ class Directory {
   };
 
   
-  int CreateNode(const std::string node, const std::string value, int options=0);        // create a node in the directory tree (permanent by default, or persistent until process disconnects if isEphemeral set). node is assigned an initial value.
+  int CreateNode(const std::string node, const std::string value, int options=0);            // create a node in the directory tree (permanent by default, or persistent until process disconnects if isEphemeral set). node is assigned an initial value.
   int SubscribeNewChild(const std::string node, int purge=0, NodeCallback *callback=NULL);   // subsribe to new child nodes created below given node. If purge set, delete existing sub-nodes. If callback set, function will be called accordingly
-  
+  int SubscribeNode(const std::string node, NodeCallback *callback=NULL);                    // subscribe to node value update.
+    
+   
   int SetValue(const std::string node, const std::string value);                         // set value of a node
   int GetValue(const std::string node, std::string &value);                              // get value of a node
   
@@ -95,9 +98,14 @@ class ControlClient {
   ~ControlClient();
   
   int sendCommand(const std::string command);
-  int getState(const std::string &currentState);
+  int getState(std::string &currentState);
 
-  private:
+  virtual void objectCreatedCallback();
+  virtual void objectDestroyedCallback();
+  virtual void objectStateChangedCallback(const std::string newState); // called when remote object state changes
+  
+  friend class ControlPrivate;
+  protected:
   ControlPrivate *dPtr; ///< private class data
 };
 
