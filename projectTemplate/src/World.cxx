d75a099eddbdff02302db06f45f15a67dbcd3556
Barthelemy <barthelemy.von.haller@cern.ch>
2015-09-18 09:46:23 +0200

    Add missing files for projectTemplate

---
+++ projectTemplate/src/World.cxx
@@ -0,0 +1,29 @@
+///
+/// @file    World.cxx
+/// @author  Barthelemy von Haller
+///
+
+#include "ProjA/World.h"
+
+#include <iostream>
+
+namespace AliceO2 {
+namespace ProjectTemplate {
+namespace ProjA {
+
+void World::greet()
+{
+  std::cout << "ProjA world!!" << std::endl;
+}
+
+int World::returnsN(int n)
+{
+
+  /// \todo This is how you can markup a todo in your code that will show up in the documentation of your project.
+  /// \bug This is how you annotate a bug in your code.
+  return n;
+}
+
+} // namespace ProjA
+} // namespace ProjectTemplate
+} // namespace AliceO2
