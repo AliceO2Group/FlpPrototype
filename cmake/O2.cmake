9c4a09ccd60dea9743a06743be313d47d3476ca3
Barthelemy <barthelemy.von.haller@cern.ch>
2015-09-22 17:27:33 +0200

    Add better support for subprojects dependencies.

--- cmake/O2.cmake
+++ cmake/O2.cmake
@@ -30,9 +30,16 @@ include(UninstallTarget)
 
 # Function to link between sub-projects
 function(link_o2_subproject subproject_name)
-    if (NOT TARGET ${subproject_name}) # target unknown because we build only this subproject, ProjA must have been built AND installed
+    #if (NOT TARGET ${subproject_name}) # target unknown
+    if(NOT PROJECT_${subproject_name}) # var unknown because we build only this subproject, ProjA must have been built AND installed
         find_package(${subproject_name} CONFIG REQUIRED)
     else () # we know the target thus we are doing a build from the top directory
         include_directories(../${subproject_name}/include)
     endif ()
 endfunction(link_o2_subproject)
+
+# Make sure we tell the topdir CMakeLists that we exist (if build from topdir)
+get_directory_property(hasParent PARENT_DIRECTORY)
+if(hasParent)
+    set(PROJECT_${PROJECT_NAME} true PARENT_SCOPE)
+endif()
