2928e8ac74daf67e27564875d3d773cf1e7c887a
Barthelemy <barthelemy.von.haller@cern.ch>
2015-09-22 08:46:14 +0200

    Add a dummy library to DataFormat as we don't use yet CMake 3.1

---
+++ DataFormat/src/DataBlock.cxx
@@ -0,0 +1,15 @@
+///
+/// @file    World.cxx
+/// @author  Barthelemy von Haller
+///
+
+#include "DataFormat/DataBlock.h"
+
+namespace AliceO2 {
+namespace ProjectTemplate {
+namespace DataFormat {
+
+
+} // namespace DataFormat
+} // namespace ProjectTemplate
+} // namespace AliceO2
