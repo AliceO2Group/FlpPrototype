bf98f172a22cf3f005e071a8ac19d2380f22d080
Barthelemy <barthelemy.von.haller@cern.ch>
2016-04-28 16:57:50 +0200

    extracted qc module to its own directory

---
+++ QualityControlModules/Example/test/testQcExample.cxx
@@ -0,0 +1,101 @@
+///
+/// @file    TestHello.cxx
+/// @author  Barthelemy von Haller
+///
+
+#include "../include/Example/ExampleTask.h"
+
+#define BOOST_TEST_MODULE hello test
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#include <assert.h>
+#include <iostream>
+
+using namespace std;
+
+BOOST_AUTO_TEST_CASE(arithmetic_test)
+{
+  int a = 1;
+  int b = 2;
+  BOOST_CHECK_NE(a, b);
+  b = a;
+  BOOST_CHECK_EQUAL(a, b);
+}
+
+namespace AliceO2 {
+namespace QualityControl {
+
+using namespace Core;
+
+namespace Test {
+class TestTask : public TaskInterface
+{
+  public:
+    TestTask(std::string name, ObjectsManager *objectsManager) : TaskInterface(name, objectsManager)
+    {
+      test = 0;
+    }
+
+    virtual ~TestTask()
+    {
+
+    }
+
+    // Definition of the methods for the template method pattern
+    virtual void initialize()
+    {
+      cout << "initialize" << endl;
+      test = 1;
+    }
+
+    virtual void startOfActivity(Activity &activity)
+    {
+      cout << "startOfActivity" << endl;
+      test = 2;
+    }
+
+    virtual void startOfCycle()
+    {
+      cout << "startOfCycle" << endl;
+    }
+
+    virtual void monitorDataBlock(DataBlock &block)
+    {
+      cout << "monitorDataBlock" << endl;
+    }
+
+    virtual void endOfCycle()
+    {
+      cout << "endOfCycle" << endl;
+    }
+
+    virtual void endOfActivity(Activity &activity)
+    {
+      cout << "endOfActivity" << endl;
+    }
+
+    virtual void Reset()
+    {
+      cout << "Reset" << endl;
+    }
+
+    int test;
+};
+
+} /* namespace Test */
+} /* namespace QualityControl */
+} /* namespace AliceO2 */
+
+//BOOST_AUTO_TEST_CASE(TestInstantiate)
+//{
+//  ObjectsManager objectsManager; // TODO we need a mock object
+//  Test::TestTask tt("asdf", &objectsManager);
+//  BOOST_CHECK_EQUAL(tt.test, 0);
+//  tt.initialize();
+//  BOOST_CHECK_EQUAL(tt.test, 1);
+//  Activity act;
+//  tt.startOfActivity(act);
+//  BOOST_CHECK_EQUAL(tt.test, 2);
+//}
+
