0b10b2b3639a83340bb47354cd5aaf84b2a8e64d
Barthelemy <barthelemy.von.haller@cern.ch>
2016-06-22 08:40:15 +0200

    add an example of beautification

--- QualityControlModules/Common/src/NonEmpty.cxx
+++ QualityControlModules/Common/src/NonEmpty.cxx
@@ -5,9 +5,11 @@
 
 #include "Common/NonEmpty.h"
 
-#include "TH1.h"
+// std
 #include <assert.h>
 #include <iostream>
+// ROOT
+#include <TH1.h>
 
 using namespace std;
 
@@ -57,25 +59,7 @@ std::string NonEmpty::getAcceptedType()
 
 void NonEmpty::beautify(MonitorObject *mo, Quality checkResult)
 {
-  // We set the background color of the mo to green if the quality returned by the check is good
-  // red otherwise (this is a bad example as several calls would overwrite each others)
-
-  if (!this->isObjectCheckable(mo)) {
-    cerr << "object not checkable" << endl;
-    return;
-  }
-
-  TH1* th1 = dynamic_cast<TH1*>(mo->getObject());
-
-  if (checkResult == Quality::Good) {
-    th1->SetFillColor(kGreen);
-  } else if (checkResult == Quality::Medium) {
-    th1->SetFillColor(kOrange);
-  } else if (checkResult == Quality::Bad) {
-    th1->SetFillColor(kRed);
-  } else {
-    th1->SetFillColor(kWhite);
-  }
+// NOOP
 }
 
 }  // namespace Checker
