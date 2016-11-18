8888de71e07defc9bec3931df444b9997eab528a
Adam Tadeusz Wegrzynek <adam.wegrzynek@cern.ch>
2016-11-18 17:01:12 +0100

    passing whole message to publisher (instead of name and value)

--- GUI/src/Publisher.cxx
+++ GUI/src/Publisher.cxx
@@ -33,27 +33,14 @@ Publisher::~Publisher()
   socket.unbind(url.c_str());
 }
 
-bool Publisher::publish(std::string &name, std::string &value)
+bool Publisher::publish(std::string &message)
 {
-  std::ostringstream ss;
-  ss << "{\"name\":\"" << name << "\","
-     << "\"value\":\"" << value << "\","
-     << "\"timestamp\":" << getTimestampMilisecs() << ",";
-  std::string jsonMessage = ss.str();
-  zmq::message_t message(jsonMessage.size());
-  memcpy(message.data(), jsonMessage.data(), jsonMessage.size());
-  if (socket.send(message)) {
+  zmq::message_t zmqMessage(message.size());
+  memcpy(zmqMessage.data(), message.data(), message.size());
+  if (socket.send(zmqMessage)) {
     return true;
   }
   return false;
-}
-
-unsigned long Publisher::getTimestampMilisecs()
-{
-  auto now = std::chrono::system_clock::now();
-  return std::chrono::duration_cast <std::chrono::nanoseconds>(
-    now.time_since_epoch()
-  ).count();
 }
 
 } // namespace GUI
