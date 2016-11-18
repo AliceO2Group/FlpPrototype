021e3e42cfcad92ea1065b406fcef77a1fdb358e
Adam Tadeusz Wegrzynek <adam.wegrzynek@cern.ch>
2016-11-18 13:41:43 +0100

    GUI: deleted parts of previous prototypes

--- GUI/ws-server/server.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace CERN\Alice\DAQ\O2;
-require_once __DIR__.'/TCP/ServerSocket.php';
-require_once __DIR__.'/Log.php';
-
-date_default_timezone_set('Europe/Zurich');
-$s = new ServerSocket();
-$s->listen();
-?>
