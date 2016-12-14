9ac246946ac1c22e68a5375a891b5ee65d57fdb9
sy-c <o2.syc@cern.ch>
2016-12-14 15:08:28 +0100

    using shared_ptr

--- DataSampling/src/InjectSamples.cxx
+++ DataSampling/src/InjectSamples.cxx
@@ -1,12 +1,12 @@
 #include <DataSampling/InjectSamples.h>
 #include <stdio.h>
 
-int injectSamples(std::vector<DataBlockContainer *> &dataBlocks) {
+int injectSamples(std::vector<std::shared_ptr<DataBlockContainer>> &dataBlocks) {
 
   unsigned int nBlocks=(int)dataBlocks.size();
   int totalSize=0;
   for (unsigned int i=0;i<nBlocks;i++) {
-    DataBlockContainer *blockContainer=dataBlocks.at(i);
+    std::shared_ptr<DataBlockContainer> blockContainer=dataBlocks.at(i);
         
     DataBlockHeaderBase *header=&blockContainer->getData()->header;   
     char *payload=blockContainer->getData()->data;
