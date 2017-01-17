26d247b7d078ff2a304f0224064a800552c8199e
Adam Tadeusz Wegrzynek <adam.wegrzynek@cern.ch>
2017-01-17 11:22:33 +0100

    GUI, format comments

--- GUI/example/3-FairOnData.cxx
+++ GUI/example/3-FairOnData.cxx
@@ -5,21 +5,22 @@
 #include "FairMQLogger.h"
 #include "FairMQProgOptions.h"
 
+/// register a handler for data arriving on "notification" channel
 FairOnData::FairOnData()
 {
-    // register a handler for data arriving on "data" channel
     OnData("notification", &FairOnData::HandleNotification);
 }
 
-// handler is called whenever a message arrives on "data", with a reference to the message and a sub-channel index (here 0)
+/// handler is called whenever a message arrives on "notification", with a reference to the message and a sub-channel index (here 0)
 bool FairOnData::HandleNotification(FairMQMessagePtr& msg, int index)
 {
     LOG(INFO) << "Notification received: \"" << std::string(static_cast<char*>(msg->GetData()), msg->GetSize()) << "\"";
+
+    // create response
     std::string *response = new std::string("{ \"executed\": 1 }");
 
-    // create message object with a pointer to the data buffer,
-    // its size,
-    // custom deletion function (called when transfer is done),
+    // create message object with a pointer to the data buffer
+    // custom deletion function (called when transfer is done)
     // and pointer to the object managing the data buffer
     FairMQMessagePtr responseMessage(NewMessage(const_cast<char*>(response->c_str()),
                                      response->length(),
