473bb28c78cd28fe117d52faa358d2b62d4a23bd
Barthelemy von Haller <barthelemy.von.haller@cern.ch>
2017-04-13 08:58:15 +0200

    Fix the FindConfiguration to allow passing the root installation directory.

--- cmake/FindConfiguration.cmake
+++ cmake/FindConfiguration.cmake
@@ -6,28 +6,27 @@
 #  Configuration_INCLUDE_DIRS - The Configuration include directories
 #  Configuration_LIBRARIES - The libraries needed to use Configuration
 #  Configuration_DEFINITIONS - Compiler switches required for using Configuration
+#
+# This script can use the following variables:
+#  Configuration_ROOT - Installation root to tell this module where to look. (it tries LD_LIBRARY_PATH otherwise)
 
 # Init
 include(FindPackageHandleStandardArgs)
 
 # find includes
-if(NOT DEFINED CONFIGURATION_INCLUDE_DIR)
 find_path(CONFIGURATION_INCLUDE_DIR Configuration.h
-           HINTS ENV LD_LIBRARY_PATH PATH_SUFFIXES "../include/Configuration" "../../include/Configuration" )
+           HINTS ${Configuration_ROOT}/include ENV LD_LIBRARY_PATH PATH_SUFFIXES "../include/Configuration" "../../include/Configuration" )
 # Remove the final "Configuration" 
 get_filename_component(CONFIGURATION_INCLUDE_DIR ${CONFIGURATION_INCLUDE_DIR} DIRECTORY)
-endif()
 set(Configuration_INCLUDE_DIRS ${CONFIGURATION_INCLUDE_DIR})
 
 # find library
-if(NOT DEFINED CONFIGURATION_LIBRARY)
-find_library(CONFIGURATION_LIBRARY NAMES Configuration HINTS ENV LD_LIBRARY_PATH)
-endif()
+find_library(CONFIGURATION_LIBRARY NAMES Configuration HINTS ${Configuration_ROOT}/lib ENV LD_LIBRARY_PATH)
 set(Configuration_LIBRARIES ${CONFIGURATION_LIBRARY})
 
 # handle the QUIETLY and REQUIRED arguments and set CONFIGURATION_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(Configuration  "Configuration could not be found. Install package Configuration."
+find_package_handle_standard_args(Configuration  "Configuration could not be found. Install package Configuration or set Configuration_ROOT to its root installation directory."
                                   CONFIGURATION_LIBRARY CONFIGURATION_INCLUDE_DIR)
 
 if(${CONFIGURATION_FOUND})
