b3d63ff4727d0af8165b54e856f31c2266e9d4a2
Barthelemy von Haller <barthelemy.von.haller@cern.ch>
2017-06-30 11:34:27 +0200

    Fix memory leak FairSampler

--- DataSampling/src/FairSampler.cxx
+++ DataSampling/src/FairSampler.cxx
@@ -5,7 +5,6 @@
 
 #include <DataFormat/DataBlock.h>
 #include "DataSampling/FairSampler.h"
-#include "DataSampling/DsInfoLogger.h"
 
 using namespace std;
 
@@ -69,36 +68,37 @@ void FairSampler::Run()
 void FairSampler::releaseData()
 {
   if (mBlock) {
-    // TODO delete elements?
-    delete mBlock;
-    mBlock = nullptr;
+    deleteBlock();
   }
 
   mBlockMutex.unlock();
 }
 
+void FairSampler::deleteBlock()
+{
+  mBlock->clear();
+  delete mBlock;
+  mBlock = nullptr;
+}
+
 bool FairSampler::HandleData(FairMQParts &parts, int /*index*/)
 {
-//  cout << "HandleData()" << endl;
-//  cout << "   number of parts : " << parts.Size() << endl;
-
-  // store the data
   if (!mBlockMutex.try_lock()) {
     return true;
   }
 
-  // TODO delete elements?
+  // reset data
   if (mBlock) {
-    delete mBlock;
+    deleteBlock();
   }
   mBlock = new std::vector<std::shared_ptr<DataBlockContainer>>();
 
+  // create new block
   auto *block = new DataBlock();
   // TODO loop over parts
   block->header = *static_cast<DataBlockHeaderBase *>(parts.At(0)->GetData());
   block->data = static_cast<char *>(parts.At(1)->GetData()); // TODO should I copy ?
-  std::shared_ptr<DataBlockContainer> containerPtr = std::make_shared<DataBlockContainer>(block);
-  mBlock->push_back(containerPtr);
+  mBlock->push_back(make_shared<DataBlockContainer>(block));
   mBlockMutex.unlock();
   // TODO we continuously receive data and trash it. This is stupid.
   // tODO we should rather get one sample, store it, wait only get another one when getData() is called.
