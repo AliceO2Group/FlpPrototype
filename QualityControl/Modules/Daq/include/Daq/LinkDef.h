0847ed7fb02a71d6ea9ebbcf6c8d6c5afdc6dd89
Barthelemy von Haller <barthelemy.von.haller@cern.ch>
2017-08-24 12:40:23 +0200

    Extracted QualityControl

--- QualityControl/Modules/Daq/include/Daq/LinkDef.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CLING__
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
-
-#pragma link C++ class AliceO2::QualityControlModules::Daq::DaqTask+;
-#pragma link C++ class AliceO2::QualityControlModules::Daq::EverIncreasingGraph+;
-
-#endif
